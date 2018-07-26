c66df70bb3fa1a7e3338d150c5f6fd61aba43ad8
gilad <giladhaim3@gmail.com>
2018-07-27 01:11:01 +0300

    yes

---
+++ rockpaperscissors.py
@@ -0,0 +1,20 @@
+print(welcome)
+pla1 = input("player's 1 choice: ")
+pla2 = input("player's 2 choice: ")
+if pla1 == "rock" and pla2 == "scissors":
+	print("player 1 won!")
+elif pla1 == "scissors" and pla2 == "paper":
+	print("player 1 won!")
+elif pla1 == "rock" and pla2 == "scissors":
+	print("player 1 won!")
+elif pla2 == "rock" and pla1 == "scissors":
+	print("player 2 won!")
+elif pla2 == "scissors" and pla1 == "paper":
+	print("player 2 won!")
+elif pla2 == "rock" and pla1 == "scissors":
+	print("player 2 won!")	
+elif pla1 == pla2
+    print("nobody won")
+else: 
+    print("you have mistyped")    
+
